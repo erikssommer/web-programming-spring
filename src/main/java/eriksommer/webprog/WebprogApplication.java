b5cadafc37d5207c98f72a87d14bd9a5d9d750be
Erik Storås Sommer <eriksommer99@gmail.com>
2021-01-14 13:52:04 +0100

    Initial commit

---
+++ src/main/java/eriksommer/webprog/WebprogApplication.java
@@ -0,0 +1,13 @@
+package eriksommer.webprog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebprogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebprogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eriksommer/webprog/WebprogApplicationTests.java
@@ -0,0 +1,13 @@
+package eriksommer.webprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
