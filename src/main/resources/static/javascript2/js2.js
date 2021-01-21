69d09234cd36dbd9f02a836d426a65ae9ab22d65
Erik Storås Sommer <eriksommer99@gmail.com>
2021-01-21 17:10:16 +0100

    Kode fra oving

---
+++ src/main/resources/static/javascript2/js2.js
@@ -0,0 +1,11 @@
+function visPersonRegister() {
+    const personregister = [];
+
+    const person1 = {
+        navn: "Line Nilsen",
+        adresse: "Osloveien 22",
+        telefonnr: "43565872"
+    }
+
+    //Fortsettelse følger
+}
