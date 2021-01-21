c930f933e57b98dc492da0d1da3bbf7b1966f1ba
Erik Storås Sommer <eriksommer99@gmail.com>
2021-01-21 16:56:24 +0100

    Løsningsforslag til ukesoppgavene javascript1

--- src/main/resources/static/javascript1/ukesoppgaver.js
+++ src/main/resources/static/javascript1/ukesoppgaver.js
@@ -1,6 +1,26 @@
+//Oppgave 1
+function heiVerden() {
+    document.write("Hei verden");
+}
+
+//Oppgave 2
+function heiVerdenAlert() {
+    alert("Hei verden");
+}
+
 //Oppgave 3
 function skrivMelding(input) {
     document.write(input);
+}
+
+//Oppgave 4
+function writeAlert(data) {
+    alert(data);
+}
+
+//Oppgave 5
+function writeConsole(data) {
+    console.log(data);
 }
 
 //Oppgave 6
@@ -13,6 +33,26 @@ function tilStorBokstav(input) {
     document.write(input.toUpperCase());
 }
 
-//Melding
+//Oppgave 8
+function skrivUtEn() {
+    const ut = 1;
+    console.log(ut);
+}
 
-//Melding2
+//Oppgave 9
+let ut = 0;
+
+function tellOpp() {
+    ut += 1;
+    console.log(ut);
+}
+
+//Oppgave 10
+let out = 0;
+
+function tellOppMelding() {
+    out += 1;
+    let meldingP1 = "Du har trykket på knappen ";
+    let meldingP2 = " ganger.";
+    console.log(meldingP1 + out + meldingP2);
+}
