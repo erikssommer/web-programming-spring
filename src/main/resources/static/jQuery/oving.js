ea960ee8ab605e970cb52c5244b09b320c6a6687
Erik Storås Sommer <eriksommer99@gmail.com>
2021-02-05 14:18:12 +0100

    Kode fra oving jquery

--- src/main/resources/static/jQuery/oving.js
+++ src/main/resources/static/jQuery/oving.js
@@ -1,3 +1,5 @@
+const liste = [];
+
 $(function () {
     $("#knapp").click(function () {
         $("#melding").html("<h2>Hallo " + $("#inn").val() + "</h2>");
@@ -6,4 +8,20 @@ $(function () {
     $("#inn").change(function () {
         $("#navn").text($("#inn").val());
     })
+
+    $("#beregn").click(function () {
+        liste.push(lagObjekt("Erik", 22, "Male"));
+
+        for (let person of liste) {
+            console.log(person.navn + " " + person.alder + " " + person.kjonn);
+        }
+    })
 });
+
+function lagObjekt(navn, alder, kjonn) {
+    return {
+        navn,
+        alder,
+        kjonn
+    };
+}
