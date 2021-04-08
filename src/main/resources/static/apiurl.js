dc12d934ac28eb468747084adc2df7f03cccc1b7
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-08 18:30:13 +0200

    Api for relasjonsdatabase 2

--- src/main/resources/static/apiurl.js
+++ src/main/resources/static/apiurl.js
@@ -8,3 +8,4 @@ export const apiLagringServer = "/api/lagringserver";
 export const apiNedtrekkslister = "/api/nedtrekkslister";
 export const apiRekursjon = "/api/rekursjon";
 export const apiRelasjonsdatabase1 = "/api/relasjonsdatabase1"
+export const apiRelasjonsdatabase2 = "/api/relasjonsdatabase2"
