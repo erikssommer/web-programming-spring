1f98f04908b1f28f8c93cdb3da63ec320ca988e8
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-14 14:15:53 +0200

    Lagt til tabeller og insatt verdier

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,3 +16,25 @@ CREATE TABLE Bil
     type  VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- Motorvogn6 tabeller (her har Motorvogn-tabellen en egen id som autogenereres)
+
+CREATE TABLE Motorvogn6
+(
+    id         INTEGER AUTO_INCREMENT NOT NULL,
+    personnr   INTEGER      NOT NULL,
+    navn       VARCHAR(255) NOT NULL,
+    adresse    VARCHAR(255) NOT NULL,
+    kjennetegn VARCHAR(255) NOT NULL,
+    merke      VARCHAR(255) NOT NULL,
+    type       VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Bil6
+(
+    id    INTEGER AUTO_INCREMENT NOT NULL,
+    merke VARCHAR(255) NOT NULL,
+    type  VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
