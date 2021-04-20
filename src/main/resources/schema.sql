60ccf81b65f1735b4ac569d0a6895751245291ec
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-20 14:39:09 +0200

    Tabell for motorvogn7

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -38,3 +38,25 @@ CREATE TABLE Bil6
     type  VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- Motorvogn7 tabeller (her har Motorvogn-tabellen en egen id som autogenereres)
+
+CREATE TABLE Motorvogn7
+(
+    id         INTEGER AUTO_INCREMENT NOT NULL,
+    personnr   VARCHAR(255) NOT NULL,
+    navn       VARCHAR(255) NOT NULL,
+    adresse    VARCHAR(255) NOT NULL,
+    kjennetegn VARCHAR(255) NOT NULL,
+    merke      VARCHAR(255) NOT NULL,
+    type       VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Bil7
+(
+    id    INTEGER AUTO_INCREMENT NOT NULL,
+    merke VARCHAR(255) NOT NULL,
+    type  VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
