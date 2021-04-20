6702eb4e1e5635c11ce4d0517b947da86cb54291
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-20 14:39:36 +0200

    Tabellverdier for motorvogn7

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -35,3 +35,23 @@ INSERT INTO Bil6 (merke, type)
 VALUES ('Audi', 'Q7');
 INSERT INTO Bil6 (merke, type)
 VALUES ('Audi', 'Q8');
+
+// Motorvogn7
+
+INSERT INTO Motorvogn7 (personnr, navn, adresse, kjennetegn, merke, type)
+VALUES ('02129823415', 'Per Hansen', 'Osloveien 82, 0375 Oslo', 'DR22322', 'Volvo', 'V70');
+INSERT INTO Motorvogn7 (personnr, navn, adresse, kjennetegn, merke, type)
+VALUES ('02129923612', 'Line Jensen', 'Askerveien 5, 1372 Asker', 'BJ22399', 'Audi', 'Q8');
+
+INSERT INTO Bil7 (merke, type)
+VALUES ('Volvo', 'V40');
+INSERT INTO Bil7 (merke, type)
+VALUES ('Volvo', 'V70');
+INSERT INTO Bil7 (merke, type)
+VALUES ('Volvo', 'V90');
+INSERT INTO Bil7 (merke, type)
+VALUES ('Audi', 'A8');
+INSERT INTO Bil7 (merke, type)
+VALUES ('Audi', 'Q7');
+INSERT INTO Bil7 (merke, type)
+VALUES ('Audi', 'Q8');
