1168dc82b5974b60e5be5fa9653ba0522ecaf5c3
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-21 11:36:15 +0200

    Insetting av verdier

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -55,3 +55,26 @@ INSERT INTO Bil7 (merke, type)
 VALUES ('Audi', 'Q7');
 INSERT INTO Bil7 (merke, type)
 VALUES ('Audi', 'Q8');
+
+// Motorvogn8
+
+INSERT INTO Motorvogn8 (personnr, navn, adresse, kjennetegn, merke, type)
+VALUES ('02129823415', 'Per Hansen', 'Osloveien 82, 0375 Oslo', 'DR22322', 'Volvo', 'V70');
+INSERT INTO Motorvogn8 (personnr, navn, adresse, kjennetegn, merke, type)
+VALUES ('02129923612', 'Line Jensen', 'Askerveien 5, 1372 Asker', 'BJ22399', 'Audi', 'Q8');
+
+INSERT INTO Bil8 (merke, type)
+VALUES ('Volvo', 'V40');
+INSERT INTO Bil8 (merke, type)
+VALUES ('Volvo', 'V70');
+INSERT INTO Bil8 (merke, type)
+VALUES ('Volvo', 'V90');
+INSERT INTO Bil8 (merke, type)
+VALUES ('Audi', 'A8');
+INSERT INTO Bil8 (merke, type)
+VALUES ('Audi', 'Q7');
+INSERT INTO Bil8 (merke, type)
+VALUES ('Audi', 'Q8');
+
+INSERT INTO Bruker8 (brukernavn, passord)
+VALUES ('Admin', 'Admin1234');
