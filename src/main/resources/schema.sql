c018ac031a86ba325b952aaebba4d7b1e55faedb
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-21 11:35:34 +0200

    Tabeller for sesjoner

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -60,3 +60,33 @@ CREATE TABLE Bil7
     type  VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- Motorvogn8 tabeller (Sesjoner: bruker er ny)
+
+CREATE TABLE Motorvogn8
+(
+    id         INTEGER AUTO_INCREMENT NOT NULL,
+    personnr   VARCHAR(255) NOT NULL,
+    navn       VARCHAR(255) NOT NULL,
+    adresse    VARCHAR(255) NOT NULL,
+    kjennetegn VARCHAR(255) NOT NULL,
+    merke      VARCHAR(255) NOT NULL,
+    type       VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Bil8
+(
+    id    INTEGER AUTO_INCREMENT NOT NULL,
+    merke VARCHAR(255) NOT NULL,
+    type  VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Bruker8
+(
+    id         INTEGER AUTO_INCREMENT NOT NULL,
+    brukernavn VARCHAR(255) NOT NULL,
+    passord    VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
