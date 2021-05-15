1c9b410d8a854f730698dd5bfa83ff6b5ba3a01e
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-15 21:00:56 +0200

    Lagt til error fallback

---
+++ src/main/java/eriksommer/webprog/ErrorHandler.java
@@ -0,0 +1,21 @@
+package eriksommer.webprog;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorHandler implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error(){
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
